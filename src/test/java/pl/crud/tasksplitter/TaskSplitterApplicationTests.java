2f10182b6c7c247e6ec3ad12d3ef9739ec988a43
przemak19 <przemak19@gmail.com>
2024-09-17 18:51:23 +0200

    Spring Boot Application initialization.

---
+++ src/test/java/pl/crud/tasksplitter/TaskSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.crud.tasksplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSplitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
